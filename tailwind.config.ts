1593329e4ae9c0d0c446f6a9539a18e24bc12272
EdiedRamos <edieisa22@gmail.com>
2024-08-26 23:52:37 -0500

    feat: add custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        // multi-step register
+        "msr-purple-100": "#845EEE",
+        "msr-purple-200": "#652CD1",
+        "msr-black": "#121826",
+        "msr-dark": "#212936",
+        "msr-gray-300": "#394150",
+        "msr-gray-200": "#4D5562",
+        "msr-gray-100": "#A1A1A9",
+        "msr-white": "#E5E7EB",
+      },
     },
   },
   plugins: [],
