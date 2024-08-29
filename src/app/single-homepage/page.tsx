ba69639b4008d285b199668070de68bf73667737
EdiedRamos <edieisa22@gmail.com>
2024-08-29 03:13:32 -0500

    feat: update styles

--- src/app/single-homepage/page.tsx
+++ src/app/single-homepage/page.tsx
@@ -3,7 +3,7 @@ import { Content, Header } from "./(components)";
 export default function SingleHomepage() {
   return (
     <div className="bg-sh-light dark:bg-sh-black min-h-screen">
-      <div className="max-w-[1200px] mx-auto">
+      <div className="max-w-[1180px] mx-auto">
         <div className="p-7">
           <Header />
         </div>
