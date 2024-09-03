86d9d6b2fb6fa567e56bead380709f610cf4d696
EdiedRamos <edieisa22@gmail.com>
2024-09-03 14:37:58 -0500

    feat: update styles

--- src/app/process-page/page.tsx
+++ src/app/process-page/page.tsx
@@ -7,7 +7,7 @@ export default function ProcessPage() {
           alt="process page background"
         />
       </div>
-      <div className="max-w-[1280px] mx-auto px-5 md:px-10">
+      <div className="max-w-[1200px] mt-10 mx-auto px-5 md:px-10">
         <h3 className="text-pp-purple text-cs-24">Our Process</h3>
         <h2 className="text-pp-dark text-cs-48 w-full max-w-[700px] font-bold">
           Easy And Perfect Solution For Your Business App
@@ -15,7 +15,9 @@ export default function ProcessPage() {
         <div className="flex flex-wrap gap-5 mt-20">
           <div className="max-w-64">
             <img src="/process-page/process-icon-1.svg" alt="" />
-            <p className="text-pp-dark text-cs-18 mt-8 mb-5">47+ Components</p>
+            <p className="text-pp-dark text-cs-18 mt-8 mb-5 border-l-[3px] border-pp-purple pl-1">
+              47+ Components
+            </p>
             <p className="text-pp-gray-200">
               Save time and build products with{" "}
               <span className="text-pp-purple">
@@ -26,7 +28,9 @@ export default function ProcessPage() {
           </div>
           <div className="max-w-64">
             <img src="/process-page/process-icon-2.svg" alt="" />
-            <p className="text-pp-dark text-cs-18 mt-8 mb-5">Documentation</p>
+            <p className="text-pp-dark text-cs-18 mt-8 mb-5 border-l-[3px] border-pp-purple pl-1">
+              Documentation
+            </p>
             <p className="text-pp-gray-200">
               Developer handoff made easy. Component and style guide
               documentation for your team in{" "}
@@ -35,7 +39,9 @@ export default function ProcessPage() {
           </div>
           <div className="max-w-64">
             <img src="/process-page/process-icon-3.svg" alt="" />
-            <p className="text-pp-dark text-cs-18 mt-8 mb-5">Design faster</p>
+            <p className="text-pp-dark text-cs-18 mt-8 mb-5 border-l-[3px] border-pp-purple pl-1">
+              Design faster
+            </p>
             <p className="text-pp-gray-200">
               Stop wasting time on creating everything from scratch for{" "}
               <span className="text-pp-purple">every new project</span> you
@@ -44,7 +50,9 @@ export default function ProcessPage() {
           </div>
           <div className="max-w-64">
             <img src="/process-page/process-icon-4.svg" alt="" />
-            <p className="text-pp-dark text-cs-18 mt-8 mb-5">Customize</p>
+            <p className="text-pp-dark text-cs-18 mt-8 mb-5 border-l-[3px] border-pp-purple pl-1">
+              Customize
+            </p>
             <p className="text-pp-gray-200">
               Easily apply your branding by changing font, color and{" "}
               <span className="text-pp-purple">component properties</span>.
