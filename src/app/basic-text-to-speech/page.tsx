58c408893a67ef6ad31a198f14a2c114b8c6d220
EdiedRamos <edieisa22@gmail.com>
2024-09-04 21:42:13 -0500

    feat: update styles

--- src/app/basic-text-to-speech/page.tsx
+++ src/app/basic-text-to-speech/page.tsx
@@ -23,17 +23,15 @@ export default function BasicTextToSpeech() {
             different voice by selecting an option from the dropdown menu.
           </p>
           <p className="text-ts-white text-cs-12 mt-8">Settings</p>
-          <div className="flex flex-wrap gap-2 py-2 rounded-lg items-center bg-ts-blue-300 max-w-80 text-ts-white">
+          <div className="flex flex-wrap gap-2 py-2 rounded-lg items-center bg-ts-blue-300 max-w-80 text-ts-white mt-2">
             <p className="text-ts-gray px-5">Voice</p>
             <select className="bg-inherit p-3 focus:bg-ts-blue-400 rounded-lg outline-none">
               <option className="bg-ts-blue-400">en-US</option>
-              <option className="bg-ts-blue-400">en-US</option>
-              <option className="bg-ts-blue-400">en-US</option>
+              <option className="bg-ts-blue-400">es-CO</option>
             </select>
             <select className="bg-inherit p-3 focus:bg-ts-blue-400 rounded-lg outline-none">
               <option className="bg-ts-blue-400">Albert</option>
-              <option className="bg-ts-blue-400">Albert</option>
-              <option className="bg-ts-blue-400">Albert</option>
+              <option className="bg-ts-blue-400">Magic</option>
             </select>
           </div>
           <div className="flex flex-wrap gap-2 bg-ts-blue-300 py-2 rounded-lg items-center mt-[18px] max-w-[350px] text-ts-white">
