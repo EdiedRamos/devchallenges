66f3e043350db0278b19e86ab8c9ef1ee6144a82
EdiedRamos <edieisa22@gmail.com>
2024-09-04 23:21:13 -0500

    feat: update styles

--- src/app/basic-text-to-speech/(components)/voiceControl.tsx
+++ src/app/basic-text-to-speech/(components)/voiceControl.tsx
@@ -2,12 +2,16 @@
 
 import { useSpeech } from "../(context)/useSpeech";
 
+function cutWord(word: string): string {
+  return word.substring(0, 10).concat(word.length > 10 ? "..." : "");
+}
+
 export function VoiceControl() {
   const { voices, currentVoices, handleCurrentLang, handleCurrentVoice } =
     useSpeech();
 
   return (
-    <div className="flex flex-wrap gap-2 py-2 rounded-lg items-center bg-ts-blue-300 max-w-80 text-ts-white mt-2">
+    <div className="flex flex-wrap gap-2 py-2 rounded-lg items-center bg-ts-blue-300 max-w-96 text-ts-white mt-2">
       <p className="text-ts-gray px-5">Voice</p>
       {voices ? (
         <select
@@ -23,7 +27,7 @@ export function VoiceControl() {
       )}
       {currentVoices ? (
         <select
-          className="bg-inherit p-3 focus:bg-ts-blue-400 rounded-lg outline-none"
+          className="bg-inherit p-3 focus:bg-ts-blue-400 rounded-lg outline-none max-w-32"
           onChange={(event) => handleCurrentVoice(event.target.value)}
         >
           {currentVoices.map((voice) => (
