a7a59694ec8511200a15378ca6a776bd28ecf2fe
EdiedRamos <edieisa22@gmail.com>
2024-09-06 23:11:48 -0500

    feat: add isActive category preview styles

--- src/app/recipe-page/(components)/categoryPreview.tsx
+++ src/app/recipe-page/(components)/categoryPreview.tsx
@@ -1,11 +1,16 @@
 import { Category } from "../(models)";
 
 interface Props {
+  isActive?: boolean;
   category: Category;
 }
-export function CategoryPreview({ category }: Props) {
+export function CategoryPreview({ isActive = false, category }: Props) {
   return (
-    <div className="border border-rp-gray rounded-xl h-16 max-w-72 flex flex-wrap items-center gap-3">
+    <div
+      className={`border border-rp-gray rounded-xl h-16 max-w-72 flex flex-wrap items-center gap-3 hover:cursor-pointer hover:opacity-80 ${
+        isActive ? "bg-rp-yellow text-rp-dark" : "text-rp-light"
+      }`}
+    >
       <img
         src={category.strCategoryThumb}
         alt=""
