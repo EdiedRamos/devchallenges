ecd9c39cfb7a30ac605bfe8080e562c5ddbbe279
EdiedRamos <edieisa22@gmail.com>
2024-09-07 02:55:30 -0500

    feat: update styles

--- src/app/recipe-page/(components)/categoryPreview.tsx
+++ src/app/recipe-page/(components)/categoryPreview.tsx
@@ -13,7 +13,7 @@ export function CategoryPreview({
   return (
     <div
       onClick={() => onSelectCategory(category)}
-      className={`border border-rp-gray overflow-hidden rounded-xl h-16 max-w-72 flex flex-wrap items-center gap-3 hover:cursor-pointer hover:opacity-80 ${
+      className={`border border-rp-gray overflow-hidden rounded-xl h-14 max-w-72 flex flex-wrap items-center gap-3 hover:cursor-pointer hover:opacity-80 ${
         isActive ? "bg-rp-yellow text-rp-dark" : "text-rp-light"
       }`}
     >
