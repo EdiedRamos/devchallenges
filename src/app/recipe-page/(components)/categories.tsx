be79608eaf6e70179a6b46641557b4d3c05902ed
EdiedRamos <edieisa22@gmail.com>
2024-09-07 02:59:51 -0500

    feat: update styles

--- src/app/recipe-page/(components)/categories.tsx
+++ src/app/recipe-page/(components)/categories.tsx
@@ -12,22 +12,24 @@ import { CategoryPreview } from "./categoryPreview";
 export function Categories() {
   const { categories, currentCategory, handleCurrentCategory } = useRecipe();
   return (
-    <section className="w-full max-w-72">
-      <h4
-        className={`${playfair_Display.className} text-cs-24 font-medium text-rp-light`}
-      >
-        Categories
-      </h4>
-      <div className="flex flex-col gap-3 mt-8">
-        {categories.map((category) => (
-          <CategoryPreview
-            onSelectCategory={handleCurrentCategory}
-            key={category.idCategory}
-            category={category}
-            isActive={currentCategory?.idCategory === category.idCategory}
-          />
-        ))}
-      </div>
-    </section>
+    <div className="w-full max-w-80 flex justify-end">
+      <section className="w-full max-w-64">
+        <h4
+          className={`${playfair_Display.className} text-cs-24 font-medium text-rp-light`}
+        >
+          Categories
+        </h4>
+        <div className="flex flex-col gap-3 mt-8">
+          {categories.map((category) => (
+            <CategoryPreview
+              onSelectCategory={handleCurrentCategory}
+              key={category.idCategory}
+              category={category}
+              isActive={currentCategory?.idCategory === category.idCategory}
+            />
+          ))}
+        </div>
+      </section>
+    </div>
   );
 }
