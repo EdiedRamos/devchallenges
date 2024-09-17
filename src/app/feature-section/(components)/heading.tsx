84f59d55db5ef6f3d3150afc707cea85bde28b93
EdiedRamos <edieisa22@gmail.com>
2024-09-16 23:41:04 -0500

    feat: update steyles

--- src/app/feature-section/(components)/heading.tsx
+++ src/app/feature-section/(components)/heading.tsx
@@ -1,7 +1,7 @@
 export function Heading() {
   return (
     <div className="py-12 flex flex-col items-center">
-      <h1 className="text-fs-white text-cs-40 font-bold max-w-[500px] text-center">
+      <h1 className="text-fs-white text-cs-40 font-medium leading-[50px] max-w-[500px] text-center">
         Quality feedbacks for your SaaS products
       </h1>
       <p className="mt-4 text-fs-gray text-cs-16">
