79f4e31e1af46f7cef5a0ebcc72e5ea24b01b418
EdiedRamos <edieisa22@gmail.com>
2024-09-16 22:03:19 -0500

    feat: implement design

--- src/app/join-our-newsletter/components/inputEmail.tsx
+++ src/app/join-our-newsletter/components/inputEmail.tsx
@@ -7,7 +7,7 @@ export function InputEmail({ children, className, ...rest }: Props) {
     <div>
       <input
         type="email"
-        className={`w-full py-5 ring-2 text-center outline-none rounded-xl placeholder:text-jo-gray placeholder:font-medium text-jo-gray focus:ring-jo-blue ${className}`}
+        className={`w-full py-5 ring-2 bg-jo-cream ring-jo-light-gray text-center outline-none rounded-xl placeholder:text-jo-gray placeholder:font-medium text-jo-gray focus:ring-jo-blue ${className}`}
         {...rest}
       />
     </div>
