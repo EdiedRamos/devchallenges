1c5ede59ab143f358dd0fc408cb12496926aec22
EdiedRamos <edieisa22@gmail.com>
2024-09-16 23:49:06 -0500

    feat: update card text leading

--- src/app/feature-section/(components)/featureCard.tsx
+++ src/app/feature-section/(components)/featureCard.tsx
@@ -12,12 +12,12 @@ type Props = { feature: FeatureI };
 
 export function FeatureCard({ feature }: Props) {
   return (
-    <article className="bg-fs-dark w-full max-w-[324px] p-6 rounded-lg hover:cursor-pointer hover:translate-y-4 transition-all">
+    <article className="bg-fs-dark w-full max-w-[300px] p-6 rounded-xl hover:cursor-pointer hover:translate-y-4 transition-all">
       {feature.Icon()}
-      <h3 className="mt-4 text-cs-20 font-medium text-fs-white">
+      <h3 className="mt-4 text-cs-20 font-medium text-fs-white leading-6">
         {feature.title}
       </h3>
-      <p className="my-4 text-fs-gray text-cs-16 max-w-[250px]">
+      <p className="my-4 text-fs-gray text-cs-16 max-w-[250px] leading-5">
         {feature.description}
       </p>
       <img
