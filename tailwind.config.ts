a75d28229d25a7473b7185fcfeb8d3dc17fbb4a1
EdiedRamos <edieisa22@gmail.com>
2024-09-19 21:22:34 -0500

    feat: register 32px font size option

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -132,6 +132,7 @@ const config: Config = {
         "cs-18": "18px",
         "cs-20": "20px",
         "cs-24": "24px",
+        "cs-32": "32px",
         "cs-36": "36px",
         "cs-40": "40px",
         "cs-48": "48px",
