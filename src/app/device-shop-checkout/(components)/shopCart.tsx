6b7275536410b39062c25b662d211950352f6ed1
EdiedRamos <edieisa22@gmail.com>
2024-09-25 23:42:47 -0500

    feat: update styles

--- src/app/device-shop-checkout/(components)/shopCart.tsx
+++ src/app/device-shop-checkout/(components)/shopCart.tsx
@@ -6,7 +6,7 @@ export function ShopCart() {
         <p className="text-dsc-gray-200">Tax</p>
         <p className="text-dsc-black">Total</p>
       </div>
-      <div className="text-dsc-gray-200 flex flex-col gap-2">
+      <div className="text-dsc-gray-200 flex flex-col gap-2 items-end">
         <p>$2597.00</p>
         <p>$623.28</p>
         <p className="text-dsc-black">$3220.28</p>
