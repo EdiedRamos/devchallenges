7850d5de34f15ea913903e6e333e1682d9153113
EdiedRamos <edieisa22@gmail.com>
2024-09-28 16:31:59 -0500

    feat: update header styles

--- src/app/bank-dashboard/(components)/header.tsx
+++ src/app/bank-dashboard/(components)/header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <header className="flex flex-wrap justify-between">
+    <header className="flex flex-wrap justify-between px-6 py-5 border-b-[1px] border-bdb-gray-200">
       <img src="/bank-dashboard/hbank-logo.svg" alt="hbank logo" />
       <div className="flex flex-wrap items-center gap-3">
         <img src="/bank-dashboard/avatar.png" alt="user image" />
