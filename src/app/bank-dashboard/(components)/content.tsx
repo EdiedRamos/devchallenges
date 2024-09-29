a597121098cf3c907b69ad04046d6be34de84d83
EdiedRamos <edieisa22@gmail.com>
2024-09-29 18:47:53 -0500

    refactor: move shopping card component to its own file

--- src/app/bank-dashboard/(components)/content.tsx
+++ src/app/bank-dashboard/(components)/content.tsx
@@ -1,35 +1,4 @@
-function ShoppingCard({
-  primaryBg = "bg-orange-200",
-  secondaryBg = "bg-purple-200",
-}: {
-  primaryBg?: string;
-  secondaryBg?: string;
-}) {
-  return (
-    <div className="w-full max-w-72 rounded-2xl overflow-hidden">
-      <div
-        className={`flex flex-wrap justify-between p-6 pb-10 font-medium ${primaryBg}`}
-      >
-        <div className="flex flex-col gap-4">
-          <p className="text-bdb-dark">Shopping Card</p>
-          <p className="text-bdb-gray-100">NAME</p>
-        </div>
-        <div className="flex flex-col gap-4 items-end">
-          <div>
-            <img src="/bank-dashboard/cpu.svg" alt="microchip card image" />
-          </div>
-          <p className="text-bdb-gray-100">Exp 26/6</p>
-        </div>
-      </div>
-      <div
-        className={`flex flex-wrap justify-between items-center p-6 text-bdb-dark font-bold ${secondaryBg}`}
-      >
-        <p className="text-cs-20">**** **** 4486</p>
-        <p className="text-cs-20">VISA</p>
-      </div>
-    </div>
-  );
-}
+import { ShoppingCard } from "./shoppingCard";
 
 function AddCard() {
   return (
--- src/app/bank-dashboard/(components)/index.ts
+++ src/app/bank-dashboard/(components)/index.ts
@@ -2,3 +2,5 @@ export * from "./header";
 export * from "./navigation";
 export * from "./aside";
 export * from "./content";
+
+export * from "./shoppingCard";
