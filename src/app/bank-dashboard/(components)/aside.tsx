95dfdba8e0c41d4285eeb7595fcca101b8c88a13
EdiedRamos <edieisa22@gmail.com>
2024-09-29 21:50:55 -0500

    feat: update aside styles

--- src/app/bank-dashboard/(components)/aside.tsx
+++ src/app/bank-dashboard/(components)/aside.tsx
@@ -3,7 +3,7 @@ import { ShoppingCard } from "./shoppingCard";
 export function Aside() {
   return (
     <aside className="w-full max-w-[332px] border-l-[1px] border-bdb-gray-200 p-6">
-      <div className="border-[1px] border-bdb-gray-200 p-6">
+      <div className="border-[1px] border-bdb-gray-200 p-6 rounded-2xl">
         <h4 className="text-bdb-white font-bold">Card details</h4>
         <div className="h-96 flex flex-col items-center justify-center">
           <ShoppingCard
@@ -30,9 +30,13 @@ export function Aside() {
           </div>
         </div>
 
-        <div className="my-6">
+        <div className="mt-6">
           <h4 className="text-bdb-white font-bold">Spending limits</h4>
           <p className="text-cs-12 mt-6">DAILY TRANSACTION LIMIT</p>
+          <div className="w-full h-2 bg-bdb-green-light rounded-full my-3">
+            <div className="w-[20%] bg-bdb-green h-2 rounded-full" />
+          </div>
+          <p>$400 spent of $2000</p>
         </div>
       </div>
     </aside>
