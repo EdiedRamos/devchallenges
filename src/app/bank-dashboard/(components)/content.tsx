8ebf6b78c743779fafe5e322244e25fb63c048ff
EdiedRamos <edieisa22@gmail.com>
2024-09-29 19:11:08 -0500

    feat: add information

--- src/app/bank-dashboard/(components)/content.tsx
+++ src/app/bank-dashboard/(components)/content.tsx
@@ -14,12 +14,27 @@ export function Content() {
         />
       </div>
       <AddCard />
-      <Info
-        text="Earnings"
-        money="12341"
-        image="/bank-dashboard/show chart.svg"
-        iconColor="bg-bdb-green"
-      />
+
+      <section className="flex flex-wrap justify-around border-[1px] border-bdb-gray-200 rounded-xl p-5">
+        <Info
+          text="Earnings"
+          money="12341"
+          image="/bank-dashboard/show chart.svg"
+          iconColor="bg-bdb-green"
+        />
+        <Info
+          text="Spendings"
+          money="4009"
+          image="/bank-dashboard/dollar coin.svg"
+          iconColor="bg-bdb-purple-200"
+        />
+        <Info
+          text="Goals"
+          money="8098"
+          image="/bank-dashboard/aim.svg"
+          iconColor="bg-bdb-orange-200"
+        />
+      </section>
     </main>
   );
 }
