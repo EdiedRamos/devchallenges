d2e4a9580e2b5ac85127ba526afdbad6a2ff63bb
EdiedRamos <edieisa22@gmail.com>
2024-09-29 20:45:06 -0500

    feat: create balance section

--- src/app/bank-dashboard/(components)/content.tsx
+++ src/app/bank-dashboard/(components)/content.tsx
@@ -3,6 +3,21 @@ import { ShoppingCard, AddCard, Info } from ".";
 export function Content() {
   return (
     <main>
+      <div className="w-full flex flex-wrap rounded-xl overflow-hidden">
+        <article className="w-[50%] bg-bdb-green-light text-bdb-dark font-medium p-5 flex flex-col gap-4">
+          <p>Your balance:</p>
+          <p className="text-cs-24 font-bold">
+            ${Number(25330).toLocaleString()}
+          </p>
+          <p>3 cards</p>
+        </article>
+        <div className="w-[50%] bg-bdb-green flex justify-end items-end p-5 bg-bdb-bg bg-no-repeat bg-right">
+          <button className="px-4 py-1 bg-bdb-white text-bdb-dark font-medium rounded-2xl">
+            Details
+          </button>
+        </div>
+      </div>
+
       <div className="flex flex-wrap gap-5">
         <ShoppingCard
           primaryBg="bg-bdb-purple-100"
